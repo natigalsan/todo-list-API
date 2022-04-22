6e2852860b75ef9ed03f62b162b560def179b71c
natigalsan <natividadgalsan@gmail.com>
2022-04-22 18:56:52 +0000

    creando la lista

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,25 @@
 import React from "react";
+import { useEffect } from "react/cjs/react.production.min";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+
 const Home = () => {
-	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	const [input, addInput] = useState([]);
+	useEffect(() => {
+		return (
+			<div>
+				<h1 className="text-center mt-5 ">TODOS</h1>
+				<div className="container d-flex justify-content-center">
+					<button onClick={addInput(...input, "")}></button>
+					<input type="text" />
+					<i class="fas fa-trash-alt"></i>
+				</div>
+			</div>
+		);
+	}, []);
 };
 
 export default Home;
