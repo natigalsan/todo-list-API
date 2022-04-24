2f8f9ce19e9b8c685a7e5a55633a6f0fa6fbf251
natigalsan <natividadgalsan@gmail.com>
2022-04-24 15:05:38 +0000

    creando las funciones

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,31 +6,80 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 
 const Home = () => {
-	const [input, setInput] = useState([]);
-	useEffect(() => {
-		console.log(input);
-	}, [input]);
-	return (
-		<div>
-			<h1 classNameName="text-center mt-5 ">TODOS</h1>
+	const [lista, setLista] = useState("");
 
-			<div classNameName="container d-flex justify-content-center">
-				<button
-					onClick={() => {
-						setInput([...input, value]);
-					}}>
-					Ok
-				</button>
-				<input
-					type="text"
-					OnChange={(e) => setInput(e.target.value)}
-					value={input}
-					placeholder="Please, write something"
-				/>
-				<i className="fas fa-trash-alt"></i>
+	const [input, setInput] = useState([]);
+	// useEffect(() => {
+	// 	console.log(lista);
+	// }, [lista]);
+
+	return (
+		<div className="container-fluid">
+			<h1>Lista de Tareas</h1>
+			<input
+				type="text"
+				onChange={(e) => setInput(e.target.value)}
+				value={input}
+				placeholder="Please, write something."
+			/>
+			<button
+				onClick={() => {
+					setLista([...lista, input]);
+				}}>
+				Ok
+			</button>
+			<hr />
+			<div className="tabla">
+				<div className="row">
+					<div className="col-1"></div>
+					<div className="col-5">
+						<div className="row">Tareas por realizar:</div>
+						<span>{input}</span>
+					</div>
+					<div className="col-3">
+						<div className="row">Eliminar:</div>
+					</div>
+					<div className="col-1"></div>
+				</div>
 			</div>
 		</div>
 	);
 };
+
+// 	<div style={{ justifyContent: "Center", alignContent: "center" }}>
+// 		<h1 className="text-center mt-5 ">Lista de Tareas</h1>
+
+// 		<div className="container d-flex justify-content-center">
+// 			<i className="far fa-times-circle"></i>
+// 			<input
+// 				type="text"
+// 				OnChange={(e) => setInput(e.target.value)}
+// 				placeholder="Please, write something"
+// 			/>
+
+// 			<button
+// 				onClick={() => {
+// 					setInput([...lista, input]);
+// 				}}>
+// 				Ok
+// 			</button>
+// 			<br />
+// 			<span>{input ? input : ""}</span>
+// 		</div>
+// 		<div className="tabla">
+// 			<table>
+// 				<tr>
+// 					<th></th>
+// 					<th>
+// 						<i className="far fa-times-circle"></i>
+// 					</th>
+// 					<tbody>
+
+// 					</tbody>
+// 				</tr>
+// 			</table>
+// 		</div>
+// 	</div>
+// );
 
 export default Home;
